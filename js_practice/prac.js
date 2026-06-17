d3ade0e351c61dc52d59d79f63d1e89aa7adca54
Vaishnavi Pednekar <vaishnavi10pednekar@gmail.com>
2026-06-17 14:41:23 +0530

    updated project

---
+++ js_practice/prac.js
@@ -0,0 +1,44 @@
+let x = "vaishnavi";
+console.log(x);
+console.log(typeof(x));
+x = false;
+console.log(typeof(x));
+
+let y = 1;
+console.log(y);
+console.log(typeof(y));
+
+let xa = 4.56;
+console.log(xa);
+console.log(typeof(xa));
+
+let ya;
+console.log(ya);
+console.log(typeof(ya));
+
+let student = {
+    name:"vaishnavi",
+    age:19,
+    degree:"BE",
+    year:"3rd",
+
+};
+
+console.log(student.name);
+console.log(student);
+console.log(student['age']);
+
+let course = ['DSA','maths','science','history','geography',8,true,8.6];
+
+console.log(course);
+console.log(typeof(course));
+console.log(course[4]);
+console.log(course[8]);
+
+createGame('Temple run');
+function createGame(games){
+    console.log("creating" + games);
+}
+
+createGame('subway suffers');
+createGame('purple palace');
