90999e0fa015a110d92faff6cdcf8244eac9d33b
Vaishnavi Pednekar <vaishnavi10pednekar@gmail.com>
2026-06-22 23:15:02 +0530

    updated project

--- js_practice/prac.js
+++ js_practice/prac.js
@@ -1,47 +1,47 @@
-// let x = "vaishnavi";
-// console.log(x);
-// console.log(typeof(x));
-// x = false;
-// console.log(typeof(x));
+console.log(x);
+console.log(typeof(x));
+x = false;
+console.log(typeof(x));
 
-// let y = 1;
-// console.log(y);
-// console.log(typeof(y));
+let y = 1;
+console.log(y);
+console.log(typeof(y));
 
-// let xa = 4.56;
-// console.log(xa);
-// console.log(typeof(xa));
+let xa = 4.56;
+console.log(xa);
+console.log(typeof(xa));
 
-// let ya;
-// console.log(ya);
-// console.log(typeof(ya));
+let ya;
+console.log(ya);
+console.log(typeof(ya));
 
-// let student = {
-//     name:"vaishnavi",
-//     age:19,
-//     degree:"BE",
-//     year:"3rd",
+let student = {
+    name:"vaishnavi",
+    age:19,
+    degree:"BE",let x = "vaishnavi";
 
-// };
+    year:"3rd",
 
-// console.log(student.name);
-// console.log(student);
-// console.log(student['age']);
+};
 
-// let course = ['DSA','maths','science','history','geography',8,true,8.6];
+console.log(student.name);
+console.log(student);
+console.log(student['age']);
 
-// console.log(course);
-// console.log(typeof(course));
-// console.log(course[4]);
-// console.log(course[8]);
+let course = ['DSA','maths','science','history','geography',8,true,8.6];
 
-// createGame('Temple run');
-// function createGame(games){
-//     console.log("creating" + games);
-// }
+console.log(course);
+console.log(typeof(course));
+console.log(course[4]);
+console.log(course[8]);
 
-// createGame('subway suffers');
-// createGame('purple palace');
+createGame('Temple run');
+function createGame(games){
+    console.log("creating" + games);
+}
+
+createGame('subway suffers');
+createGame('purple palace');
 
 console.log(a);
 const a = 10;
